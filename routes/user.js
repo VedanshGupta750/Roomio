ad1328d2fb02017550051c27aee73324ff3a51b8
VedanshGupta2533 <guptavp@rknec.edu>
2026-02-02 16:06:41 +0530

    login page

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/user.js");
+const passport = require("passport")
 
 router.get('/signup' , (req , res)=>{
     res.render("users/signup.ejs" )
@@ -23,5 +24,14 @@ router.post('/signup' , async (req, res)=>{
     }
     
 });
+
+router.get('/login' , (req ,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post('/login' ,passport.authenticate("local" , {failureRedirect: '/login' , failureFlash:true}) ,async (req , res)=>{
+    req.flash("success" , "Welcome to Roomio, You are logged in!");
+    res.redirect("/listings");
+})
 
 module.exports = router;
