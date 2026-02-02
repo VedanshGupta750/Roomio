b65327c9e1563e5c08cee170aca55a8ae181ab53
VedanshGupta2533 <guptavp@rknec.edu>
2026-02-02 19:13:02 +0530

    redirectUrl empty resolve

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport")
+const {saveRedirectUrl} = require("../middleware.js");
+
+
 
 router.get('/signup', (req, res) => {
     res.render("users/signup.ejs")
@@ -33,12 +36,14 @@ router.post('/signup', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+   
     res.render("users/login.ejs");
 });
 
-router.post('/login', passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+router.post('/login', saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
     req.flash("success", "Welcome to Roomio, You are logged in!");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res) => {
