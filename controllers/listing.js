52a2ef3991c9978837a5240e04d6063fd1427c77
VedanshGupta2533 <guptavp@rknec.edu>
2026-02-08 16:46:27 +0530

    Co-ordinatine storing

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,6 +1,7 @@
 const Listing = require('../models/listing.js');
-
-
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 module.exports.index = async (req, res) => {
     const listings = await Listing.find({});
     res.render("listings/listings.ejs", { listings });
@@ -34,13 +35,21 @@ module.exports.renderEditListingForm = async (req, res) => {
 };
 
 module.exports.addNewListing = async (req, res) => {
+  let response = await geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1,
+})
+  .send()
+
     let url = req.file.path;
     let filename = req.file.filename;
     console.log(url  ,     "..." , filename);
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = {url , filename};
-    await newListing.save();
+    newListing.geometry = response.body.features[0].geometry;
+    let savedListing = await newListing.save();
+    console.log(savedListing)
     req.flash("success" , "New listing created successfully!");
     res.redirect("/listings");
 };
